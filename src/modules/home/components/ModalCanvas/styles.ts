a8dfdaff51369e587947194311772545496b4b3f
Oleksii Holodniak <holodniakwork@gmail.com>
2023-10-13 22:23:34 +0300

    first commit

---
+++ src/modules/home/components/ModalCanvas/styles.ts
@@ -0,0 +1,11 @@
+"use client";
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+  width: 100%;
+  height: 400px;
+  canvas {
+    width: 100%;
+    height: 100%;
+  }
+`;
---
+++ src/modules/home/components/index.ts
@@ -0,0 +1,2 @@
+export { default as Introduction } from "./Introduction/Introduction";
+export { default as ModalCanvas } from "./ModalCanvas/ModalCanvas";
